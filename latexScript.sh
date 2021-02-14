c80d025c30be3db032d68ad00734a99596c5a2fb
luis ponce <lushop@localhost.localdomain>
2021-02-14 02:06:37 -0500

    Done .. i guess

--- latexScript.sh
+++ latexScript.sh
@@ -20,6 +20,96 @@ function ctrl_c(){
 	tput cnorm; exit 1
 }
 
+function printTable(){
+
+    local -r delimiter="${1}"
+    local -r data="$(removeEmptyLines "${2}")"
+
+    if [[ "${delimiter}" != '' && "$(isEmptyString "${data}")" = 'false' ]]
+    then
+        local -r numberOfLines="$(wc -l <<< "${data}")"
+
+        if [[ "${numberOfLines}" -gt '0' ]]
+        then
+            local table=''
+            local i=1
+
+            for ((i = 1; i <= "${numberOfLines}"; i = i + 1))
+            do
+                local line=''
+                line="$(sed "${i}q;d" <<< "${data}")"
+
+                local numberOfColumns='0'
+                numberOfColumns="$(awk -F "${delimiter}" '{print NF}' <<< "${line}")"
+
+                if [[ "${i}" -eq '1' ]]
+                then
+                    table="${table}$(printf '%s#+' "$(repeatString '#+' "${numberOfColumns}")")"
+                fi
+
+                table="${table}\n"
+
+                local j=1
+
+                for ((j = 1; j <= "${numberOfColumns}"; j = j + 1))
+                do
+                    table="${table}$(printf '#| %s' "$(cut -d "${delimiter}" -f "${j}" <<< "${line}")")"
+                done
+
+                table="${table}#|\n"
+
+                if [[ "${i}" -eq '1' ]] || [[ "${numberOfLines}" -gt '1' && "${i}" -eq "${numberOfLines}" ]]
+                then
+                    table="${table}$(printf '%s#+' "$(repeatString '#+' "${numberOfColumns}")")"
+                fi
+            done
+
+            if [[ "$(isEmptyString "${table}")" = 'false' ]]
+            then
+                echo -e "${table}" | column -s '#' -t | awk '/^\+/{gsub(" ", "-", $0)}1'
+            fi
+        fi
+    fi
+}
+
+function removeEmptyLines(){
+
+    local -r content="${1}"
+    echo -e "${content}" | sed '/^\s*$/d'
+}
+
+function repeatString(){
+
+    local -r string="${1}"
+    local -r numberToRepeat="${2}"
+
+    if [[ "${string}" != '' && "${numberToRepeat}" =~ ^[1-9][0-9]*$ ]]
+    then
+        local -r result="$(printf "%${numberToRepeat}s")"
+        echo -e "${result// /${string}}"
+    fi
+}
+
+function isEmptyString(){
+
+    local -r string="${1}"
+
+    if [[ "$(trimString "${string}")" = '' ]]
+    then
+        echo 'true' && return 0
+    fi
+
+    echo 'false' && return 1
+}
+
+function trimString(){
+
+    local -r string="${1}"
+    sed 's,^[[:blank:]]*,,' <<< "${string}" | sed 's,[[:blank:]]*$,,'
+}
+
+
+
 
 function helpPanel(){
 	echo -e "\n${redColour}[!] Uso: latexScript\n${endColour}"
@@ -60,7 +150,7 @@ function listTemplateFiles(){
 
 
   if [ "$(cat ~/.LaTexScript/lista_archivos | wc -l)" == "1" ];then
-    echo "No hay archivos."     
+	  echo -e "\n${redColour}[!] Primero has un latexScript.sh -s ...\n${endColour}"
     return 1
   fi
  
@@ -69,16 +159,26 @@ function listTemplateFiles(){
   else
     lista_archivos=$(cat ~/.LaTexScript/lista_archivos | grep "$type_file")
   fi
-  
-  echo '' > tmp.aux
+ 
+
+  echo 'Nombre$Tipo$direccion' > tmp.aux
 
   for val in $lista_archivos; do
     echo $val >> tmp.aux
   done
-
   
+  if [ "$type_file" = "THM" ]; then
+    echo -ne "${redColour}"
+  elif [ "$type_file" = "HTB" ]; then
+    echo -ne "${greenColour}"
+  elif [ "$type_file" = "ALL" ]; then
+    echo -ne "${blueColour}"
+  fi
+  printTable '$' "$(cat tmp.aux)"
+  echo -ne "${endColour}"
 
 
+  rm tmp.aux
 
   tput cnorm; exit 0
 
