0f649a972becc7c6064f4757bd04b90f4c92ed38
luis ponce <lushop@localhost.localdomain>
2021-02-13 22:02:11 -0500

    Delete setup1 - Added HTB pdf - Added THM pdf

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-variableHTB="$(pwd)/HTB"
-variableTHM="$(pwd)/THM"
-
-sed -i "s|DIRECCION_ACTUAL|$variableHTB|g" "$variableHTB/template.tex"
-sed -i "s|DIRECCION_ACTUAL|$variableTHM|g" "$variableTHM/template.tex"
-
-
